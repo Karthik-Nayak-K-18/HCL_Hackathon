e51764b7f4ac4b17bccc8b6f65dbe6717c0cc11b
Karthik Nayak K <nayanakarthiknayak@gmail.com>
2025-06-21 16:01:05 +0530

    updated main.tf

---
+++ main.tf
@@ -0,0 +1,48 @@
+provider "aws" {
+  region = "us-east-1" 
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16" 
+
+  tags = {
+    Name = "karthik-my-vpc"
+  }
+}
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-east-1a" 
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "karthik-public-subnet"
+  }
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "karthik-my-igw"
+  }
+}
+
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    Name = "karthik-public-route-table"
+  }
+}
+
+resource "aws_route_table_association" "public_subnet_association" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_route_table.id
+}
